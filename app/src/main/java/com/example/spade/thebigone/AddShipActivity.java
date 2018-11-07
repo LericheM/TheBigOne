e30ec449b0e7edf48a410c1b6255b99fb8406a60
Mathew Leriche <ml529@njit.edu>
2018-11-07 17:18:56 -0500

    Adding the base for add ship activitiy, menu items, functions and xml items

--- app/src/main/java/com/example/spade/thebigone/AddShipActivity.java
+++ app/src/main/java/com/example/spade/thebigone/AddShipActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class AddShipActivity extends AppCompatActivity {
 
@@ -16,6 +18,37 @@ public class AddShipActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+    }
+
+    public void addActivity(View view){
+        EditText lineET = findViewById(R.id.shipLine);
+        EditText nameET = findViewById(R.id.shipName);
+        EditText codeEt = findViewById(R.id.shipCode);
+        EditText regionET = findViewById(R.id.shipRegion);
+        EditText lengthET = findViewById(R.id.shipLength);
+        EditText priceET = findViewById(R.id.shipPrice);
+        EditText imageET = findViewById(R.id.shipImage);
+        EditText gratET = findViewById(R.id.shipGrat);
+
+        String line = lineET.getText().toString();
+        String name = nameET.getText().toString();
+        String code = codeEt.getText().toString();
+        String region = regionET.getText().toString();
+        int length = Integer.valueOf(lengthET.getText().toString());
+        double price = Double.valueOf(priceET.getText().toString());
+        String urlString = imageET.getText().toString();
+        double gratuity = Double.valueOf(gratET.getText().toString());
+
+        TextView tv = findViewById(R.id.bottomView);
+
+        Cruise newShip = new Cruise(line,name,code,region,length,price,gratuity,urlString);
+        try {
+            CruiseList cruiseL = CruiseList.getInstance();
+            cruiseL.add(newShip);
+        }catch(Exception e){
+            tv.append(e.toString());
+        }
 
     }
 
